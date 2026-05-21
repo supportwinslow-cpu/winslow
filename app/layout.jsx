2d9d3c539506917413eefa2f81b56f5bcbd6b1c2
winslow.12 <supportwinslow@gmail.com>
2026-05-21 10:38:30 +0530

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import { Inter, Bebas_Neue } from "next/font/google";
+import Script from "next/script";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
@@ -26,6 +27,8 @@ export const metadata = {
   description: "Premium Car Accessories",
 };
 
+const META_PIXEL_ID = "992699016482921";
+
 export default function RootLayout({ children }) {
   return (
     <html
@@ -33,6 +36,32 @@ export default function RootLayout({ children }) {
       className={`${inter.variable} ${bebas.variable} h-full antialiased`}
     >
       <body className="min-h-screen flex flex-col bg-black text-white">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+
         <AuthProvider>
           <CartProvider>
             <Navbar />
